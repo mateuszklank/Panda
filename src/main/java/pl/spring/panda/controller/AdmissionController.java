d2c4e68e58202005bde08864ff45fad8f982c2ca
Mateusz Klank <quarionek@gmail.com>
2023-09-20 20:01:59 +0200

    Fix: AdmissionController, AdmissionRepository

--- src/main/java/pl/spring/panda/controller/AdmissionController.java
+++ src/main/java/pl/spring/panda/controller/AdmissionController.java
@@ -8,6 +8,7 @@ import pl.spring.panda.model.Admission;
 import pl.spring.panda.repository.AdmissionRepository;
 import pl.spring.panda.repository.DoctorRepository;
 import pl.spring.panda.repository.PatientRepository;
+import pl.spring.panda.service.AdmissionService;
 
 import java.util.List;
 
@@ -16,75 +17,39 @@ import java.util.List;
 @RequestMapping("/api")
 public class AdmissionController {
 
-    @Autowired
-    private PatientRepository patientRepository;
+    private final AdmissionService admissionService;
 
-    @Autowired
-    private AdmissionRepository admissionRepository;
-
-    @Autowired
-    private DoctorRepository doctorRepository;
+    @Autowired AdmissionController(AdmissionService admissionService) {
+        this.admissionService = admissionService;
+    }
 
     @GetMapping("/patients/{patientId}/admissions")
     public ResponseEntity<List<Admission>> getAllAdmissionsByPatientId(@PathVariable(value = "patientId") Long patientId) {
-        if (!patientRepository.existsById(patientId)) {
-            throw new IllegalStateException("Not found patient with id " + patientId);
-        }
-
-        List<Admission> admissions = admissionRepository.findByPatientId(patientId);
-        return new ResponseEntity<>(admissions, HttpStatus.OK);
+        return admissionService.getAllAdmissionsByPatientId(patientId);
     }
 
     @GetMapping("/admissions/{admissionId}")
     public ResponseEntity<Admission> getAdmissionById(@PathVariable(value = "admissionId") Long admissionId) {
-        Admission admission = admissionRepository.findById(admissionId)
-                .orElseThrow(() -> new IllegalStateException("Not found admission with id " + admissionId));
-
-        return new ResponseEntity<>(admission, HttpStatus.OK);
+        return admissionService.getAdmissionById(admissionId);
     }
 
     @PostMapping("/patients/{patientId}/{doctorId}/admissions")
     public ResponseEntity<Admission> createAdmission(@PathVariable(value = "patientId") Long patientId, @PathVariable(value = "doctorId") Long doctorId, @RequestBody Admission admissionRequest) {
-
-        patientRepository.findById(patientId).map(patient -> {
-            admissionRequest.setPatient(patient);
-            return true;
-        }).orElseThrow(() -> new IllegalStateException("Not found patient with id " + patientId));
-
-        doctorRepository.findById(doctorId).map(doctor -> {
-            admissionRequest.setDoctor(doctor);
-            return true;
-        }).orElseThrow(() -> new IllegalStateException("Not found doctor with id " + doctorId));
-
-        Admission admission = admissionRepository.save(admissionRequest);
-
-        return new ResponseEntity<>(admission, HttpStatus.CREATED);
+        return admissionService.createAdmission(patientId, doctorId, admissionRequest);
     }
 
     @PutMapping("/admissions/{admissionId}")
-    public ResponseEntity<Admission> updateAdmission(@PathVariable("admissionId") long admission_id, @RequestBody Admission admissionRequest) {
-        Admission admission = admissionRepository.findById(admission_id)
-                .orElseThrow(() -> new IllegalStateException("Admission with id " + admission_id + " not found"));
-
-        admission.setDiagnosis(admissionRequest.getDiagnosis());
-
-        return new ResponseEntity<>(admissionRepository.save(admission), HttpStatus.OK);
+    public ResponseEntity<Admission> updateAdmission(@PathVariable("admissionId") Long admission_id, @RequestBody Admission admissionRequest) {
+        return admissionService.updateAdmission(admission_id, admissionRequest);
     }
 
     @DeleteMapping("/admissions/{admissionId}")
-    public ResponseEntity<HttpStatus> deleteAdmission(@PathVariable("admissionId") long admission_id) {
-        admissionRepository.deleteById(admission_id);
-
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<HttpStatus> deleteAdmission(@PathVariable("admissionId") Long admission_id) {
+        return admissionService.deleteAdmission(admission_id);
     }
 
     @DeleteMapping("/patients/{patientId}/admissions")
     public ResponseEntity<List<Admission>> deleteAllAdmissionsOfPatient(@PathVariable(value = "patientId") Long patient_id) {
-        if (!patientRepository.existsById(patient_id)) {
-            throw new IllegalStateException("Not found patient with id " + patient_id);
-        }
-
-        admissionRepository.deleteByPatientId(patient_id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return admissionService.deleteAllAdmissionsOfPatient(patient_id);
     }
 }
--- src/main/java/pl/spring/panda/repository/AdmissionRepository.java
+++ src/main/java/pl/spring/panda/repository/AdmissionRepository.java
@@ -2,11 +2,13 @@ package pl.spring.panda.repository;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.spring.panda.model.Admission;
 import pl.spring.panda.model.Patient;
 
 import java.util.List;
 
+@Repository
 public interface AdmissionRepository extends JpaRepository<Admission, Long> {
 
     List<Admission> findByPatientId(Long patientId);
