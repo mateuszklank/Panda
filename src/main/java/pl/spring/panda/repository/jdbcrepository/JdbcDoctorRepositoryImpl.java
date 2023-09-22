1ed7cae18b5eedf8d084d50e775367d4f5134384
Mateusz Klank <quarionek@gmail.com>
2023-09-22 14:19:10 +0200

    Fix: JdbcDoctorRepositoryImpl queries to zjdbc_doctor table

--- src/main/java/pl/spring/panda/repository/jdbcrepository/JdbcDoctorRepositoryImpl.java
+++ src/main/java/pl/spring/panda/repository/jdbcrepository/JdbcDoctorRepositoryImpl.java
@@ -17,20 +17,20 @@ public class JdbcDoctorRepositoryImpl implements JdbcDoctorRepository {
 
     @Override
     public int save(JdbcDoctor doctor) {
-        return jdbcTemplate.update("INSERT INTO doctor (first_name, last_name, specialty) VALUES(?,?,?)",
+        return jdbcTemplate.update("INSERT INTO zjdbc_doctor (first_name, last_name, specialty) VALUES(?,?,?)",
                 new Object[] { doctor.getFirst_name(), doctor.getLast_name(), doctor.getSpecialty() });
     }
 
     @Override
     public int update(JdbcDoctor doctor) {
-        return jdbcTemplate.update("UPDATE doctor SET first_name=?, last_name=?, specialty=? WHERE id=?",
+        return jdbcTemplate.update("UPDATE zjdbc_doctor SET first_name=?, last_name=?, specialty=? WHERE id=?",
                 new Object[] { doctor.getFirst_name(), doctor.getLast_name(), doctor.getSpecialty(), doctor.getId() });
     }
 
     @Override
     public JdbcDoctor findById(Long id) {
         try {
-            JdbcDoctor doctor = jdbcTemplate.queryForObject("SELECT * FROM doctor WHERE id=?",
+            JdbcDoctor doctor = jdbcTemplate.queryForObject("SELECT * FROM zjdbc_doctor WHERE id=?",
                     BeanPropertyRowMapper.newInstance(JdbcDoctor.class), id);
 
             return doctor;
@@ -41,23 +41,23 @@ public class JdbcDoctorRepositoryImpl implements JdbcDoctorRepository {
 
     @Override
     public int deleteById(Long id) {
-        return jdbcTemplate.update("DELETE FROM doctor WHERE id=?", id);
+        return jdbcTemplate.update("DELETE FROM zjdbc_doctor WHERE id=?", id);
     }
 
     @Override
     public List<JdbcDoctor> findAll() {
-        return jdbcTemplate.query("SELECT * from doctor", BeanPropertyRowMapper.newInstance(JdbcDoctor.class));
+        return jdbcTemplate.query("SELECT * from zjdbc_doctor", BeanPropertyRowMapper.newInstance(JdbcDoctor.class));
     }
 
     @Override
     public List<JdbcDoctor> findByLastNameContaining(String lastName) {
-        String q = "SELECT * from doctor WHERE last_name ILIKE '%" + lastName + "%'";
+        String q = "SELECT * from zjdbc_doctor WHERE last_name ILIKE '%" + lastName + "%'";
 
         return jdbcTemplate.query(q, BeanPropertyRowMapper.newInstance(JdbcDoctor.class));
     }
 
     @Override
     public int deleteAll() {
-        return jdbcTemplate.update("DELETE from doctor");
+        return jdbcTemplate.update("DELETE from zjdbc_doctor");
     }
 }
